686ea39533a69b9ff8c47a3fd183da828abbcc17
Mahir Tasin Islam <light.g117@gmail.com>
2025-01-09 12:49:59 +0600

    added hero list and styles

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
 import {Hero} from '../hero';
 import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
 
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, NgFor],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero:Hero = {
-    id:0,
-    name: 'Winston'
-  };
-
-  get heroNameUppercase(): string {
-    return this.hero?.name?.toUpperCase() || 'UNKNOWN';
+  heroes:Hero[] = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+  
+  
 
 }
