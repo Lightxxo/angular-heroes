37e6b0b8eb61dda713271cad2a3b9c1326993fa4
Mahir Tasin Islam <light.g117@gmail.com>
2025-01-09 13:36:12 +0600

    Refactor Heroes Component

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
-import {Hero} from '../hero';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {HEROES} from '../mock-heroes';
-
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [FormsModule, CommonModule, NgFor],
+  imports: [FormsModule, CommonModule, NgFor, HeroDetailComponent, NgIf],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes:Hero[] = HEROES;
+  heroes: Hero[] = HEROES;
   selectedHero?: Hero;
+
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
-  
-
 }
